b3731b1c1caf1ec0b0aa8b798ae9d3099d52ca5a
Dominic Groth <dominic.groth@messerli.ch>
2019-03-26 14:35:26 +0100

    Add tests

---
+++ LinqToRest/IResourceRetriever.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Messerli.LinqToRest
+{
+    public interface IResourceRetriever
+    {
+        Task<T> RetrieveResource<T>(Uri uri);
+
+        Task<object> RetrieveResource(Type type, Uri uri);
+    }
+}
---
+++ LinqToRest/ResourceRetriever.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Messerli.ServerCommunication;
+
+namespace Messerli.LinqToRest
+{
+    public class ResourceRetriever : IResourceRetriever
+    {
+        public Task<T> RetrieveResource<T>(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<object> RetrieveResource(Type type, Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
