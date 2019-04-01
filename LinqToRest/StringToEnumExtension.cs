171f45bb12aad47ca4492a535af7517eeb5d4dd7
Dominic Groth <dominic.groth@messerli.ch>
2019-04-01 16:26:43 +0200

    Improve naming

--- LinqToRest/StringToEnumExtension.cs
+++ LinqToRest/StringToEnumExtension.cs
@@ -14,12 +14,12 @@ namespace Messerli.LinqToRest
                 throw new ArgumentException($"{type.Name} is not an enum type!");
             }
 
-            var tryParse = typeof(StringToEnumExtension).GetMethod(nameof(ParseToEnumElement), new Type[] {typeof(string) })?.MakeGenericMethod(type)
+            var parse = typeof(StringToEnumExtension).GetMethod(nameof(ParseToEnumElement), new Type[] {typeof(string) })?.MakeGenericMethod(type)
                            ?? throw new MissingMethodException();
 
             try
             {
-                return tryParse.Invoke(null, new object[] { candidate });
+                return parse.Invoke(null, new object[] { candidate });
             }
             catch (TargetInvocationException e)
             {
@@ -35,7 +35,7 @@ namespace Messerli.LinqToRest
         public static T ParseToEnumElement<T>(this string candidate) where T : struct
         {
             var parsed = candidate.TryParseEnum<T>();
-            
+
             return parsed.Match(none: false, some: @enum => true)
                 ? parsed.Match(none: default(T), some: @enum => @enum)
                 : throw new InvalidEnumArgumentException(candidate);
