d910cb048d1ac2c4480c4fcfb7ba1f3276ed6f76
Jan Nils Ferner <jan.ferner@messerli.ch>
2019-04-02 16:56:33 +0200

    Implement parts of builder

--- LinqToRest/QueryProviderBuilder.cs
+++ LinqToRest/QueryProviderBuilder.cs
@@ -1,13 +1,61 @@
 using System;
+using System.Net.Http;
+using JetBrains.Annotations;
 using QueryProviderBase = Messerli.QueryProvider.QueryProvider;
 
 namespace Messerli.LinqToRest
 {
     public class QueryProviderBuilder
     {
+        private HttpClient _httpClient = new HttpClient();
+        [CanBeNull] private Uri _uri;
+
+        public QueryProviderBuilder Root(Uri uri)
+        {
+            _uri = uri;
+            return this;
+        }
+        
+        public QueryProviderBuilder HttpClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            return this;
+        }
+        
         public QueryProviderBase Build()
         {
-            throw new NotImplementedException();
+            var resourceRetriever = CreateResourceRetriever();
+            var queryBinderFactory = CreateQueryBinderFactory();
+            if (_uri is null)
+            {
+                throw new QueryProviderBuilderException($"Root uri was not configured. Call .{nameof(Root)}(...) before .{nameof(Build)}().");
+            }
+            return new QueryProvider(resourceRetriever, queryBinderFactory, _uri);
+        }
+
+        private static QueryableFactory CreateQueryableFactory()
+        {
+            return (type, uri) =>
+            {
+                throw new NotImplementedException();
+            };
+        }
+
+        private ResourceRetriever CreateResourceRetriever()
+        {
+            var queryableFactory = CreateQueryableFactory();
+            return new ResourceRetriever(_httpClient, queryableFactory);
+        }
+
+        private QueryBinderFactory CreateQueryBinderFactory()
+        {
+            var entityValidator = CreateEntityValidator();
+            return () => new QueryBinder(entityValidator);
+        }
+
+        private static EntityValidator CreateEntityValidator()
+        {
+            return new EntityValidator();
         }
     }
 }
