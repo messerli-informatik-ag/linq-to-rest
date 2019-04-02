9c14b2493987a891370dfff95f942a78d5d7087b
Jan Nils Ferner <jan.ferner@messerli.ch>
2019-04-02 17:13:48 +0200

    Add test

--- LinqToRest.Test/QueryProviderBuilderTest.cs
+++ LinqToRest.Test/QueryProviderBuilderTest.cs
@@ -1,4 +1,5 @@
 using System;
+using RichardSzalay.MockHttp;
 using Xunit;
 
 namespace Messerli.LinqToRest.Test
@@ -19,6 +20,20 @@ namespace Messerli.LinqToRest.Test
             var queryProvider = builder.Root(new Uri("https://www.example.com")).Build();
             
             Assert.NotNull(queryProvider);
+        } 
+        
+        [Fact]
+        public void HttpClientCanBeConfigured()
+        {
+            var builder = new QueryProviderBuilder();
+            var httpClient = new MockHttpMessageHandler().ToHttpClient();
+            var queryProvider = builder
+                .HttpClient(httpClient)
+                .Root(new Uri("https://www.example.com"))
+                .Build();
+            
+            Assert.NotNull(queryProvider);
         }
+        
     }
 }
