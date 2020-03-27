e73066e11bdce1676d1c59243f66536e7f950ea4
Dominic Groth <whitedragon@dragonlabs.ch>
2020-03-27 12:08:26 +0100

    Add static Pluralizer

---
+++ LinqToRest/Pluralizer.cs
@@ -0,0 +1,22 @@
+﻿namespace Messerli.LinqToRest
+{
+    internal static class Pluralizer
+    {
+        public static string Pluralize(string noun)
+            // Todo: search NuGet for Pluralisation
+            => noun switch
+            {
+                _ when noun.EndsWithConsonantAndY() => noun[..^1] + "ies",
+                _ => noun + "s",
+            };
+
+        private static bool EndsWithConsonantAndY(this string noun)
+            => noun.EndsWith('y') && noun[^1].IsConsonant();
+
+        private static bool IsConsonant(this char letter)
+            => !letter.IsVowel();
+
+        private static bool IsVowel(this char letter)
+            => "aeiou".Contains(letter);
+    }
+}
