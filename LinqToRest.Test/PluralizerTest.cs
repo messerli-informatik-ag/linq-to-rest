9c1759e9eadf00bced2ee4238c0d40f026769613
Dominic Groth <whitedragon@dragonlabs.ch>
2020-03-30 07:50:35 +0200

    Add plzuralization tests

---
+++ LinqToRest.Test/PluralizerTest.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace Messerli.LinqToRest.Test
+{
+    public class PluralizerTest
+    {
+        [Theory]
+        [MemberData(nameof(GetPluralizeData))]
+        public void PluralizesCorrect(string singular, string plural)
+        {
+            var result = Pluralizer.Pluralize(singular);
+            Assert.Equal(plural, result);
+        }
+
+
+        public static TheoryData<string, string> GetPluralizeData()
+            => new TheoryData<string, string>
+            {
+                { "name", "names" },
+                { "distribution", "distributions" },
+                { "library", "libraries" },
+                { "boy", "boys" },
+                { "child", "children" },
+                { "horse", "horses" },
+                { "cash", "cash" },
+                { "clothing", "clothing" },
+                { "energy", "energy" },
+                { "wildlife", "wildlife" },
+                { "tornado", "tornadoes" },
+                { "die", "dice" },
+                { "pickaxe", "pickaxes" },
+            };
+    }
+}
