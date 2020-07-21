bf28bde6d322597e5f66797777608ad663695518
Mathias Fischler <mathias.fischler@messerli.ch>
2020-07-21 09:55:49 +0200

    Remove EF specific comment

--- LinqToRest/Async/IAsyncQueryProvider.cs
+++ LinqToRest/Async/IAsyncQueryProvider.cs
@@ -15,9 +15,6 @@ namespace Messerli.LinqToRest.Async
     ///         Defines method to execute queries asynchronously that are described by an IQueryable object.
     ///     </para>
     ///     <para>
-    ///         The service lifetime is <see cref="ServiceLifetime.Scoped" />. This means that each
-    ///         <see cref="DbContext" /> instance will use its own instance of this service.
-    ///         The implementation may depend on other services registered with any lifetime.
     ///         The implementation does not need to be thread-safe.
     ///     </para>
     /// </summary>
